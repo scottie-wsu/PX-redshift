276b16ce3aca45d8e69babf2b41f3995aa4bd39e
scottie-wsu <18698422@student.westernsydney.edu.au>
2020-10-17 19:27:03 +1100

    fix method name change when no script change + old methodfile not deleting
    Allowed method name to be changed without reuploading a script. Also added useful error messages when a user tries to save a method with no new script uploaded. Fixed old method file not deleting from main scripts folder after a method is updated with a new script

---
+++ app/Http/Requests/MethodsCreateRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use App\Rules\PyScript;
+use Illuminate\Foundation\Http\FormRequest;
+
+class MethodsCreateRequest extends FormRequest
+{
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		// only allow updates if the user is logged in
+		return backpack_auth()->check();
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'method_name' => 'required|min:1|max:255',
+			'python_script_path' => ['required','file', new PyScript],
+		];
+	}
+
+	/**
+	 * Get the validation attributes that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function attributes()
+	{
+		return [
+			//
+		];
+	}
+
+	/**
+	 * Get the validation messages that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function messages()
+	{
+		return [
+			'method_name' => 'Method name is a required field and must be less than 255 characters.',
+			'python_script_path' => 'File must be a valid .py file.',
+		];
+	}
+}
