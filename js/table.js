ee4c145b121b13ecd42f99db34a46c84fdac6276
scottie-wsu <18698422@student.westernsydney.edu.au>
2020-10-22 12:51:02 +1100

    updated files for server compatibility
    server uses the root/{js/css} folder, whereas local development server uses public/{js/css} folder

--- js/table.js
+++ js/table.js
@@ -24,11 +24,10 @@ return 'I can see the CHILD ROW!!!';
     ],
     "order": [[1, 'asc']]
 		});
-
 			$('#historyTableOuter tbody').on('click', 'td.details-control', function () {
 				var tr = $(this).closest('tr');
 				var row = tableOuter.row( tr );
- 
+
 				if ( row.child.isShown() ) {
 					row.child.hide();
 					tr.removeClass('shown');
@@ -43,17 +42,17 @@ return 'I can see the CHILD ROW!!!';
 /* $(document).ready(function() {
 		function searchByColumn(tableOuter){
 			var defaultSearch = 0 // Job Name
-			
+
 			$(document).on('change', '#search-column', function() {
 				defaultSearch = this.value;
 			});
-			
+
 			$(document).on('keyup', '#search-by-column', function(){
 				tableOuter.search('').columns().search('').draw();
 				tableOuter.column(defaultSearch).search(this.value).draw();
 			});
 		}
-		
+
     var tableOuter = $('#historyTableOuter').DataTable( {
 			"searching": true,
 			"fixedHeader": true,
@@ -72,45 +71,65 @@ return 'I can see the CHILD ROW!!!';
 											<input class="form-control" type="text" id="search-by-column" placeholder="Search...">\
 											</div>\
 										</div>');
-		
+
 		searchByColumn(tableOuter);
-		
+
 } ); */
 
 $(document).ready(function() {
-		function searchByColumn(table){
-			var defaultSearch = 0 // Galaxy ID
-			
-			$(document).on('change', '#search-column', function() {
-				defaultSearch = this.value;
-			});
-			
-			$(document).on('keyup', '#search-by-column', function(){
-				table.search('').columns().search('').draw();
-				table.column(defaultSearch).search(this.value).draw();
-			});
+
+	
+
+		var defaultSearch = [];
+		var searchField= [];
+
+				
+			function searchByColumn(searchTable, p){
+				defaultSearch[p] = 0;
+				$(document).on('change', '#search-column'+p, function(){
+					defaultSearch[p] = this.value;
+					searchTable.search('').columns().search('').draw();
+					searchTable.column(defaultSearch[p]).search(searchField[p]).draw();
+					
+				});
+				$(document).on('keyup click', '#search-by-column'+p, function(){
+					searchField[p] = this.value;
+				  searchTable.search('').columns().search('').draw();
+					searchTable.column(defaultSearch[p]).search(this.value).draw();
+				});
 		}
 		
-    var table = $('#historyTableInner').DataTable( {
+		function hideColumns(table, p){
+			//hide columns 5-18
+			$(document).on('change', '#hideCheckbox'+p , function() {
+			table.columns([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]).visible(!$(this).is(':checked'));
+		});
+		}
+	
+		
+	
+		
+		var table=[];
+		for (var i=0; i < numTables; i++) {
+		table[i] = $('#historyTableInner'+i).DataTable( {
+			
 			"searching": true,
-			"fixedHeader": true,
+
 			"responsive": true,
 			/* "order": [[0, 'asc'], [16, 'asc']], */
 			"orderFixed":{
-				"post":[[0, 'asc']]
+				"pre":[[0, 'asc']]
+
 			},
 			/* "orderFixed": [[15, 'asc']], */
-			
 			/* "rowGroup": { "startRender":null, "endRender":null,  "dataSrc":15}, */
-
-			"dom": '<"filterSearch">rtip'
-
+			// <"filterSearch">
+			"dom": 'rtip'
 		});
-		
+		}
 
-		
-		
-		$("div.filterSearch").html('<div class="row">\
+/* 		$("div.filterSearch").html('<div class="row">\
+
 											<div class="col-md-5">\
 											<select class="form-control" id="search-column">\
 												<option value="0">Galaxy ID</option>\
@@ -135,13 +154,15 @@ $(document).ready(function() {
 											<div class="col-md-6">\
 											<input class="form-control" type="text" id="search-by-column" placeholder="Search...">\
 											</div>\
-										</div>');
+
+										</div>'); */
+
 /* 		$("div.alwaysGroup").html('<label for="alwaysGroupCheckBox">\
 															<input type="checkbox" name="alwaysGroupCheckBox" id="alwaysGroupCheckBox"\
 															value="groupByID" checked> Always Group By Galaxy ID\
-															</label>');		 */						
-		
-		
+															</label>');		 */
+
+
 /* 		function alwaysGroup(table){
 				if(document.getElementById('alwaysGroupCheckBox').checked){
 					table.rowGroup().enable().draw();
@@ -151,16 +172,22 @@ $(document).ready(function() {
 					table.rowGroup().disable().draw();
 					table.order.draw();
 				}
-			} 
-			
+			}
+
 			$('#alwaysGroupCheckBox').on('click', function(){
 				alwaysGroup(table);
 			});
 		 */
+
 		
 
-
-		searchByColumn(table);
+		for (var j=0; j < numTables; j++) {
+			searchByColumn(table[j], j);
+		}
+		
+		for (var j=0; j < numTables; j++) {
+			hideColumns(table[j], j);
+		}
 		
 
 } );
